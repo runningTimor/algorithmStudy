a7c526622c0047a9b629d40d54032f4f6b17cc04
renjie <renjie@cug.edu.cn>
2022-05-15 19:59:11 +0800

    子数组最大累加和

---
+++ src/MaxSubSum.java
@@ -0,0 +1,20 @@
+/**
+ * 子数组最大累加和
+ */
+public class MaxSubSum {
+
+    public static int process(int[] array){
+        int current = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int a :array){
+             current+=a;
+             max = Math.max(max,current);
+             if (current<0){
+                 current =0;
+             }
+        }
+
+        return max;
+    }
+}
