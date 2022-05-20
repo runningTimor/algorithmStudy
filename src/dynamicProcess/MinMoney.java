3345eeafc00e45014267bdf5265b1f3c4a2234ca
renjie <renjie@cug.edu.cn>
2022-05-20 22:09:15 +0800

    动态规划-主播人气晋升最小花费

--- src/dynamicProcess/MinMoney.java
+++ src/dynamicProcess/MinMoney.java
@@ -10,11 +10,11 @@ public class MinMoney {
      * @param multi
      * @param del     固定花费
      * @param start 开始人气
-     * @param aim     目标人气
+     * @param end     目标人气
      * @return
      */
-    public static int getMinMoney(int add, int multi, int del, int start, int aim) {
-        return process(add,multi,del,start,aim,0,2*aim,((aim-start)/2)*add);
+    public static int getMinMoney(int add, int multi, int del, int start, int end) {
+        return process(add,multi,del,start,end,0,2*end,((end-start)/2)*add);
 
     }
 
@@ -52,9 +52,41 @@ public class MinMoney {
         return Math.min(pAdd, Math.min(pMulti,pDel));
     }
 
+    /**
+     * 动态规划版本
+     * @return
+     */
+    public static int dp(int add, int multi, int del, int start, int end){
+
+        //运算过程中人气值不会超过目标人气2倍
+        int limitAim= end<<1;
+        //找到一个普通解全部由add方法组成，花费比此方法高的尝试即可舍弃
+        int limitCost = ((end -start)>>1)*add;
+
+        int[][] dp = new int[limitCost+1][limitAim+1];
+        for (int preCost = 0; preCost<=limitCost;preCost++){
+            for (int aim = 0 ; aim<=limitCost;aim++){
+                if (start==aim){
+                    dp[preCost][aim]= preCost;
+                }else {
+                    dp[preCost][aim]= Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        for (int preCost = 0; preCost<=limitCost;preCost++){
+            for (int aim = 0 ; aim<=limitCost;aim++){
+
+
+            }
+        }
+
+        return dp[0][end];
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(getMinMoney(2,10,1,0,20));
+        System.out.println(dp(2,10,1,0,20));
     }
 
 }
