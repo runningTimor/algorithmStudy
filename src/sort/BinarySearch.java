98c5cb4564bfd6d68d2f899e1a11e7cfe26b9c32
renjie <renjie@cug.edu.cn>
2022-05-24 14:57:32 +0800

    二分法

---
+++ src/sort/BinarySearch.java
@@ -0,0 +1,62 @@
+package sort;
+
+import com.sun.org.apache.xml.internal.serializer.ElemDesc;
+
+//有序数组二分查找
+public class BinarySearch {
+
+    public static int searchIndex(int[] array, int searchNum, int startIndex, int EndIndex) {
+        int midIndex = startIndex + ((EndIndex - startIndex) >> 1);
+        if (array[midIndex] == searchNum) {
+            return midIndex;
+        }
+        if (startIndex == EndIndex) {
+            return -1;
+        }
+        if (array[midIndex] > searchNum) {
+            return searchIndex(array, searchNum, startIndex, midIndex - 1);
+        } else {
+            return searchIndex(array, searchNum, midIndex + 1, EndIndex);
+        }
+    }
+
+    //在有序数组中查找大于等于searchNum中最小的数字
+    public static int searchNearestIndex(int[] array, int searchNum, int startIndex, int EndIndex) {
+        int searchIndex = EndIndex;
+        while (startIndex <= EndIndex) {
+            int midIndex = startIndex + ((EndIndex - startIndex) >> 1);
+            if (array[midIndex] >= searchNum) {
+                searchIndex = midIndex;
+                EndIndex = midIndex - 1;
+            } else {
+                startIndex = midIndex + 1;
+            }
+        }
+        return searchIndex;
+    }
+
+    //array数组无序，相邻的数一定不相等
+    public static int findLocalMin(int[] array, int startIndex, int EndIndex) {
+        int midIndex = startIndex + ((EndIndex - startIndex) >> 1);
+        if (array[midIndex]<array[midIndex+1]&&array[midIndex]<array[midIndex-1]){
+            return array[midIndex];
+        }else {
+            if (array[midIndex-1]<array[midIndex]){
+                return findLocalMin(array,startIndex,midIndex);
+            }else {
+
+            return findLocalMin(array,midIndex,EndIndex);
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4, 3, 2, 1, -1, 10};
+//        System.out.println(searchIndex(array, 5, 0, array.length - 1));
+//        System.out.println(searchNearestIndex(array, 5, 0, array.length - 1));
+        System.out.println(findLocalMin(array,0,array.length-1));
+
+    }
+}
--- src/sort/N2Sort.java
+++ src/sort/N2Sort.java
@@ -1,7 +1,7 @@
 package sort;
 
-//选择排序、冒泡排序
-public class SelectSort {
+//选择排序、冒泡排序、插入排序（时间复杂度N平方）
+public class N2Sort {
 
     //不稳定
     public static void selectSort(int[] array) {
@@ -27,11 +27,26 @@ public class SelectSort {
 
         for (int i = array.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (array[j]>array[j+1]){
-                        swap(array,j,j+1);
+                if (array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
                 }
             }
         }
+    }
+
+    public static void insertSort(int[] array) {
+        if (array == null || array.length < 2) {
+            return;
+        }
+
+        for (int i = 1; i < array.length; i++)
+            for (int j = i; j > 0; j--) {
+                if (array[j - 1] > array[j]) {
+                    swap(array, j, j-1);
+                } else {
+                    break;
+                }
+            }
     }
 
 
@@ -45,7 +60,8 @@ public class SelectSort {
     public static void main(String[] args) {
         int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 //        selectSort(array);
-        bubbleSort(array);
+//        bubbleSort(array);
+        insertSort(array);
         System.out.println(array);
     }
 
