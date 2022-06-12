5c97ba465f67b75708da8a7ac7b6d9254dc45d76
renjie <renjie@cug.edu.cn>
2022-06-12 15:46:42 +0800

    动态规划+斜率优化

--- src/unsort/MaxSubSum.java
+++ src/unsort/MaxSubSum.java
@@ -5,18 +5,17 @@ package unsort;
  */
 public class MaxSubSum {
 
-    public static int process(int[] array){
+    public static int process(int[] array) {
         int current = 0;
         int max = Integer.MIN_VALUE;
 
-        for (int a :array){
-             current+=a;
-             max = Math.max(max,current);
-             if (current<0){
-                 current =0;
-             }
+        for (int a : array) {
+            current += a;
+            max = Math.max(max, current);
+            if (current < 0) {
+                current = 0;
+            }
         }
-
         return max;
     }
 }
