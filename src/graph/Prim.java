573f2487b75127901c1655eea5b9d91b2220150e
renjie <renjie@shmaas.cn>
2026-03-30 17:20:07 +0800

    最小生成树 P算法fix

--- src/graph/Prim.java
+++ src/graph/Prim.java
@@ -15,21 +15,28 @@ public class Prim {
     public static Set<Edge> prim(Graph graph) {
         Set<Edge> res = new HashSet<>();
         Set<Node> nodeSet = new HashSet<>();
+        Set<Edge> edgeSet = new HashSet<>();
         Queue<Edge> queue = new PriorityQueue<>();
         for (Node node : graph.nodeMap.values()) {
             if (nodeSet.contains(node)) {
                 continue;
             }
+            nodeSet.add(node);
             for (Edge edge : node.edges) {
-                queue.add(edge);
+                if (!edgeSet.contains(edge)) {
+                    queue.add(edge);
+                }
             }
             while (!queue.isEmpty()) {
                 Edge edge = queue.poll();
                 Node toNode = edge.toNode;
                 if (!nodeSet.contains(toNode)) {
                     nodeSet.add(toNode);
+                    res.add(edge);
                     for (Edge nextEdge : toNode.edges) {
-                        queue.add(nextEdge);
+                        if (!edgeSet.contains(edge)) {
+                            queue.add(nextEdge);
+                        }
                     }
                 }
             }
